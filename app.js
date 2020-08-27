88dd964aab1e745edf76483fef9839b99e40fd3f
LibertySky <hello@libertyskygraphics.com>
2020-08-27 09:11:43 -0400

    posts form vorking

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const contactContent =
 	'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
-const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -21,13 +20,14 @@ app.listen(3000, function () {
 	console.log('Server started on port 3000');
 });
 
+let posts = [];
+
 app.get('/', function (req, res) {
 	console.log(posts.title);
 	res.render('home', {
 		pageTitle: 'Home',
 		pageParagraph: homeStartingContent,
-		postTitle: posts.title,
-		postBody: posts.content,
+		posts: posts,
 	});
 });
 
